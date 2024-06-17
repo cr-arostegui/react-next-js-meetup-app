82547b7c8a13b2d594b9b2eca7ad4a4e85f9d9e2
Cristhian Arostegui <cr.arostegui@gmail.com>
2024-06-17 09:42:04 -0400

    Add custom components & CSS modules

---
+++ pages/[meetupId]/index.js
@@ -0,0 +1,14 @@
+import MeetupDetail from '../../components/meetups/MeetupDetail';
+
+function MeetupDetails() {
+  return (
+    <MeetupDetail
+      image="https://www.meetingplace.es/wp-content/uploads/2022/02/Galeri%CC%81a-Home-2-1.jpg"
+      title="A First Meetup"
+      address="Some Street 5, Some City"
+      description="The meetup description"
+    />
+  );
+}
+
+export default MeetupDetails;
