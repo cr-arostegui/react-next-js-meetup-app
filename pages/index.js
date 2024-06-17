a92ef53ad70143caec0ab831c51d26821a5ae062
Cristhian Arostegui <cr.arostegui@gmail.com>
2024-06-17 09:25:37 -0400

    Fix link in main navigation, use _app file to wrap it into layout component

--- pages/index.js
+++ pages/index.js
@@ -1,28 +1,26 @@
 import MeetupList from '../components/meetups/MeetupList';
 
 const DUMMY_MEETUPS = [
-    {
-        id: 'm1',
-        title: 'A first meetup',
-        image: 'https://www.meetingplace.es/wp-content/uploads/2022/02/Galeri%CC%81a-Home-2-1.jpg',
-        address: 'Some address 5, 12345 Some City',
-        description: 'This is a first meetup',
-    },
-    {
-        id: 'm2',
-        title: 'A second meetup',
-        image: 'https://www.meetingplace.es/wp-content/uploads/2022/02/Galeri%CC%81a-Home-2-1.jpg',
-        address: 'Some address 5, 12345 Some City',
-        description: 'This is a second meetup',
-    },
-]
+  {
+    id: 'm1',
+    title: 'A first meetup',
+    image:
+      'https://www.meetingplace.es/wp-content/uploads/2022/02/Galeri%CC%81a-Home-2-1.jpg',
+    address: 'Some address 5, 12345 Some City',
+    description: 'This is a first meetup',
+  },
+  {
+    id: 'm2',
+    title: 'A second meetup',
+    image:
+      'https://www.meetingplace.es/wp-content/uploads/2022/02/Galeri%CC%81a-Home-2-1.jpg',
+    address: 'Some address 5, 12345 Some City',
+    description: 'This is a second meetup',
+  },
+];
 
 function HomePage() {
-
-  return (
-    <MeetupList  meetups={DUMMY_MEETUPS}/>
-  );
+  return <MeetupList meetups={DUMMY_MEETUPS} />;
 }
-
 
 export default HomePage;
