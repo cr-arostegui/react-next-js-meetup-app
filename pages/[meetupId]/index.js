c88c667069b21eb82109ec8556bbbc8d9e16d73b
Cristhian Arostegui <cr.arostegui@gmail.com>
2024-06-18 07:53:42 -0400

    Work with SSG data fetching and preparing paths with getStaticPaths and working with fallback pages

--- pages/[meetupId]/index.js
+++ pages/[meetupId]/index.js
@@ -11,4 +11,41 @@ function MeetupDetails() {
   );
 }
 
+export async function getStaticPaths() {
+  return {
+    fallback: false,
+    paths: [
+      {
+        params: {
+          meetupId: 'm1'
+        }
+      },
+      {
+        params: {
+          meetupId: 'm2'
+        }
+      }
+    ]
+  }
+}
+
+export async function getStaticProps(context) {
+  // fetch data for single meetup
+  const meetupId = context.params.meetupId;
+
+  console.log('meetupId', meetupId);
+
+  return {
+    props: {
+      meetupData: {
+        image: "https://www.meetingplace.es/wp-content/uploads/2022/02/Galeri%CC%81a-Home-2-1.jpg",
+        id: meetupId,
+        title: "A First Meetup",
+        address: "Some Street 5, Some City",
+        description: "The meetup description",
+      }
+    }
+  }
+}
+
 export default MeetupDetails;
